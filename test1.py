137f5a4969e10d9837e962eeb51978629593850a
sanye1991 <380871411@qq.com>
2021-03-01 11:14:43 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+# 本脚由CD现任三爷编写，用于Python课程！
+# 三爷QQ:380871411
+# KEEP ALIVE, KEEP STUDYING
+
+
+if __name__ == '__main__':
+    print('welcome to my python phase1')
